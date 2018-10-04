fffdbad761a3a88925ddd707c523cdef49980cb7
okmonn <35838332+okmonn@users.noreply.github.com>
2018-10-04 11:14:09 +0900

    gs;

--- DirectX12/DirectX12/Source/Main.cpp
+++ DirectX12/DirectX12/Source/Main.cpp
@@ -29,6 +29,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	LoadImg(b, "Material/img/sample2.png");
 
 	RotateBorn(q, v);
+	/*func::RotateBorn(q, "âEòr", DirectX::XMMatrixRotationZ(0.6f));
+	func::RotateBorn(q, "ç∂òr", DirectX::XMMatrixRotationZ(-0.6f));*/
 
 	while (func::CheckMsg() && CheckKey(INPUT_ESCAPE) != true)
 	{
